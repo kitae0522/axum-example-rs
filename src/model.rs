81d5cca343940e9f854b169ac26eb5aaf039b531
kitae0522 <kitae040522@gmail.com>
2024-12-29 00:01:22 +0900

    feat: something...

---
+++ src/model.rs
@@ -0,0 +1,25 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Default)]
+pub struct Pagination {
+    pub offset: Option<usize>,
+    pub limit: Option<usize>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CreateTodo {
+    pub text: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct UpdateTodo {
+    pub text: Option<String>,
+    pub completed: Option<bool>,
+}
+
+#[derive(Debug, Serialize, Clone)]
+pub struct Todo {
+    pub id: Option<String>,
+    pub text: String,
+    pub completed: bool,
+}
